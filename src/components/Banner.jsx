0f1608a129444b7c94c55c77a34fd43b1865e1cd
Shoaib Ahmad <shoaib.ahmad8786@gmail.com>
2025-05-16 16:50:38 +0530

    Added vercel.json and updated login page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { assets } from '../assets/assets';
+import React from "react";
+import { assets } from "../assets/assets";
+import bannerImg from "../assets/dinnerbell.jpg";
 
 const Banner = () => {
   return (
     <div>
-    <img className="bannerImg" src={assets.main_banner_bg} alt="" />
-      <h1 className='text-3xl text-center px-2 mt-2'>Gorakhpur's Best Restaurant</h1>
+      <img className="bannerImg rounded-b-3xl" src={bannerImg} alt="" />
+      <h1 className="text-3xl text-center px-2 mt-10 leading-8">
+        <span className="font-bold"> Dinner Bell </span> <br />
+        <span className="text-xl font-medium">
+          {" "}
+          Gorakhpur's First Food Delivery and Dining App
+        </span>
+      </h1>
     </div>
-  )
-}
+  );
+};
 
 export default Banner;
