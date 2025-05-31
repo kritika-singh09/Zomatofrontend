dd96ea996cd7446d86398be2e250f0654672a981
Shoaib Ahmad <shoaib.ahmad8786@gmail.com>
2025-05-31 15:29:50 +0530

    Tried to fix vercel issue again

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteCompression()],
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  },
+  plugins: [react(), tailwindcss()],
+  // resolve: {
+  //   dedupe: ["react", "react-dom"],
+  // },
   server: {
     port: 5173,
     host: true, // Add this to listen on all local IPs
     https: false,
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "react-vendor";
-            if (id.includes("firebase")) return "firebase";
-            if (id.includes("jspdf") || id.includes("html2canvas"))
-              return "pdf";
-            if (id.includes("react-icons")) return "icons";
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks(id) {
+  //         if (id.includes("node_modules")) {
+  //           if (id.includes("react")) return "react-vendor";
+  //           if (id.includes("firebase")) return "firebase";
+  //           if (id.includes("jspdf") || id.includes("html2canvas"))
+  //             return "pdf";
+  //           if (id.includes("react-icons")) return "icons";
+  //           return "vendor";
+  //         }
+  //       },
+  //     },
+  //   },
+  // },
 });
